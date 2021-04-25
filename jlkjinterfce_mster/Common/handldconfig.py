b3b811235cc6ebce07feb5ef280486c1239818ee
yanjianzao2020 <yanjianzao2020@163.com>
2021-04-25 20:09:16 +0800

    增加config配置的读取

--- jlkjinterfce_mster/Common/handldconfig.py
+++ jlkjinterfce_mster/Common/handldconfig.py
@@ -8,25 +8,19 @@ class hand_config():
         self.con.read(cur_path+'/'+filename, encoding='utf-8')
         self.dict_items = dict(self.con.items(section_name))
         return self.dict_items[key_name]
-    def set_config(self,section_name,key_name,filename,update_value):
+    def set_config(self,section_name,key_name,filename,update_value,reset_section_name=None,**kwargs):
         self.read_config(section_name='App',key_name='token',filename="config.ini")
-        self.con['table'] = {'order_th': '订单号,申请人,状态', 'user_th': '用户名,权限,状态'}
-        self.con.set('App', 'token', 'qwer1234')
+        if reset_section_name :
+            self.con[reset_section_name] = {**kwargs}
+        self.con.set(section_name, key_name,update_value)
         self.fo = open(filename, 'w', encoding='UTF-8')
         self.con.write(self.fo)
         self.fo.close()
-
 if __name__ == '__main__':
     hand = hand_config()
-    hand.set_config(section_name='App',key_name='token',filename="config.ini",update_value='jksdhgiderisoghioer')
+    hand.set_config(section_name='App',key_name='token',filename="config.ini",update_value='jksdhgiderisoghioer',reset_section_name='order',myarg2="xiaobei", myarg3=3)
     print(hand.read_config(section_name='App',key_name='token',filename="config.ini"))
-'''
-config['table'] = {'order_th': '订单号,申请人,状态', 'user_th': '用户名,权限,状态'}
-config.set('App', 'token', 'qwer1234') #对section中的option进行设置
-fo = open(path, 'w', encoding='UTF-8')  # 重新创建配置文件
-config.write(fo)  # 数据写入配置文件
-fo.close()
-'''
+
 
 
 
